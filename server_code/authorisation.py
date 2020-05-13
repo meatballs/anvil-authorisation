ffc7e6f86778bfd7d11424f1e843704de3493481
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-13 10:24:55 +0000

    Saved version

--- server_code/authorisation.py
+++ server_code/authorisation.py
@@ -3,33 +3,45 @@ from anvil.tables import app_tables
 import functools
 import anvil.users
 
-  
-class authentication_required:
+__version__ = "0.1.0"
+    
+          
+def authentication_required(func):
     """A decorator to ensure only a valid user can call a server function"""
-    def __call__(self, func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         if anvil.users.get_user() is None:
             raise ValueError("Authentication required")
         else:
             return func(*args, **kwargs)
+    return wrapper
 
           
-class authorisation_required:
+def authorisation_required(permissions):
     """A decorator to ensure a user has sufficient permissions to call a server function"""
-    def __init__(self, permissions):
-        if isinstance(permissions, str):
-            permissions = [permissions]
-        self.required_permissions = set(permissions)
-        
-    def __call__(self, func, *args, **kwargs):
-        user = anvil.users.get_user()
-        if user is None:
-            raise ValueError("Authentication required")
-        user_permissions = set([
-            permission["name"]
-            for role in user["roles"]
-            for permission in role["permissions"]
-        ])
-        if not self.required_permissions.issubset(user_permissions):
-            raise ValueError("Authorisation required")
-        else:
-            return func(*args, **kwargs)
+    def decorator(func):       
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            user = anvil.users.get_user()
+            if user is None:
+                raise ValueError("Authentication required")
+            if isinstance(permissions, str):
+                required_permissions = set([permissions])
+            else:
+                required_permissions = set(permissions)
+            try:
+                user_permissions = set([
+                    permission["name"]
+                    for role in user["roles"]
+                    for permission in role["permissions"]
+                ])
+            except TypeError:
+                raise ValueError("Authorisation required")
+              
+            if not required_permissions.issubset(user_permissions):
+                raise ValueError("Authorisation required")
+            else:
+                return func(*args, **kwargs)        
+        return wrapper
+
+    return decorator
