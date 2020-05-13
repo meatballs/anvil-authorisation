9a4aafb66a932bb7b220065d9c1a0761be358b5f
Owen Campbell <owen.campbell@tanti.org.uk>
2020-05-13 11:28:15 +0100

    Fix formatting

--- server_code/authorisation.py
+++ server_code/authorisation.py
@@ -1,25 +1,28 @@
+import functools
+
 import anvil.users
 from anvil.tables import app_tables
-import functools
-import anvil.users
 
 __version__ = "0.1.0"
-    
-          
+
+
 def authentication_required(func):
     """A decorator to ensure only a valid user can call a server function"""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if anvil.users.get_user() is None:
             raise ValueError("Authentication required")
         else:
             return func(*args, **kwargs)
+
     return wrapper
 
-          
+
 def authorisation_required(permissions):
     """A decorator to ensure a user has sufficient permissions to call a server function"""
-    def decorator(func):       
+
+    def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             user = anvil.users.get_user()
@@ -30,18 +33,21 @@ def authorisation_required(permissions):
             else:
                 required_permissions = set(permissions)
             try:
-                user_permissions = set([
-                    permission["name"]
-                    for role in user["roles"]
-                    for permission in role["permissions"]
-                ])
+                user_permissions = set(
+                    [
+                        permission["name"]
+                        for role in user["roles"]
+                        for permission in role["permissions"]
+                    ]
+                )
             except TypeError:
                 raise ValueError("Authorisation required")
-              
+
             if not required_permissions.issubset(user_permissions):
                 raise ValueError("Authorisation required")
             else:
-                return func(*args, **kwargs)        
+                return func(*args, **kwargs)
+
         return wrapper
 
     return decorator
